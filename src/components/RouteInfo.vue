04c197b3bc3dc092d2493745e7fe3bc01169b68d
Michael Bradley <Michael.Bradley@CodeSmartInc.com>
2018-03-17 21:36:22 -0700

    added RouteInfo.vue component

---
+++ src/components/RouteInfo.vue
@@ -0,0 +1,13 @@
+<template>
+<div>
+    <h2>The current route is:</h2>
+    <div>name: {{$route.name}} path: {{$route.path}}</div>
+</div>
+  
+</template>
+<script>
+export default {
+  name: 'route-info',
+
+}
+</script>
